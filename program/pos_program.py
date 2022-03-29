061ed3a55ea4b8f652981612f7c9bb72d08d7171
CalamariKing <callum.macdiarmid@icloud.com>
2022-03-29 14:17:18 +1300

    Added 2d list and allows for user input

--- program/pos_program.py
+++ program/pos_program.py
@@ -5,6 +5,17 @@ with open('menu.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         food_menu.append(row)
-for i in range(len(food_menu)):
-    menu_display = str(food_menu[i]).replace("'","").replace(",", "-").replace("[", "").replace("]", "")
+
+print("FOOD MENU:")
+for i in range(1, len(food_menu)):
+    menu_display = str(food_menu[i]).replace("'","").replace(",", "-").replace("[", "").replace("]", "").replace("- - -", "-").replace("- -", "-")
     print(str(i) + ": " + menu_display)
+
+input_num = input("\nWhat item number do you want to select?\n")
+try:
+    selected_item = food_menu[int(input_num) + 1][0]
+except:
+    print("sorry, but you need to select a number that is within the range of the menu.")
+
+
+
